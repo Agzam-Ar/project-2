253050606529786b03ce841a668a60b5718a604d
TempName <agzamkin2006@gmail.com>
2025-03-26 18:39:19 +0300

    Added Icons list

---
+++ src/static/Icons.js
@@ -0,0 +1,19 @@
+import { MdSpeed } from "react-icons/md";
+import { HiSelector } from "react-icons/hi";
+import { IoMdCheckmark } from "react-icons/io";
+import { WiTime2 } from "react-icons/wi";
+import { MdExpandMore } from "react-icons/md";
+import { PiArrowElbowDownLeftDuotone } from "react-icons/pi";
+import { MdLowPriority } from "react-icons/md";
+
+const Icons = {
+	time: <WiTime2/>,
+	difficulty: <MdSpeed/>,
+	priority: <MdLowPriority/>,
+	dropdown: <HiSelector/>,
+	checkmark: <IoMdCheckmark/>,
+	expand: <MdExpandMore/>,
+	enter: <PiArrowElbowDownLeftDuotone/>,
+};
+
+export default Icons;
