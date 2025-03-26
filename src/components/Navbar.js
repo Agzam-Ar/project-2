68fcd46458b78ac883ed9565a579f5afce7136d8
TempName <agzamkin2006@gmail.com>
2025-03-26 18:23:14 +0300

    Added Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+
+import styles from "./Navbar.module.css";
+import Link from 'next/link';
+
+const Navbar = () => {
+
+	const pathname = usePathname()
+	const linkButton = (text, url, icon) => {
+		return (<Link href={`/${url}`} className={`${(url==='' ? pathname==='/' : pathname.startsWith(`/${url}`)) ? styles.active : ""} ${styles.item}`}>
+					<div className={styles["content-box"]}>{icon}<span>{text}</span></div>
+    			</Link>);
+	};
+
+    return (<div className={styles.navbar}>
+    	<div className={styles.content}>
+    		<div className="flex">
+				<Link href="/"  className={styles.head}>Project 2</Link>
+				{linkButton("Home", "", null)}
+				{linkButton("Materials", "materials", null)}
+    		</div>
+    		<div className="flex">
+				<div className="flex items-center">
+					<input className={styles.search} type="text" name="" placeholder="поиск" id="navbar-search"/>
+				</div>
+			</div>
+		</div>
+    </div>);
+};
+
+
+export default Navbar;
