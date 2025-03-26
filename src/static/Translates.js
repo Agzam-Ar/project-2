7c9fc7afab352806688b9f423f482eecaba0bd4d
TempName <agzamkin2006@gmail.com>
2025-03-26 19:06:00 +0300

    Create Translates.js

---
+++ src/static/Translates.js
@@ -0,0 +1,31 @@
+
+
+const Translates = {
+	difficulties: {
+	    easy: "Легко",
+	    normal: "Нормально",
+	    hard: "Сложно",
+	},
+	durations: {
+	    fast: "Быстро",
+	    normal: "Нормально",
+	    long: "Долго",   
+	},
+	priority: {
+	    base: "Основное",
+	    advanced: "Расширенное",
+	    extra: "Дополнительное",   
+	},
+
+	navbar: {
+		materials: "Статьи",
+		practice: "Примеры",
+	},
+
+	quiz: {
+		check: "Проверить"
+	},
+}
+
+
+export default Translates;
