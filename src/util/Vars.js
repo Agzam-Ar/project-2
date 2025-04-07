92295b0ddfbe62a541d0f1e2af893a5e06385213
TempUser <temp@mail>
2025-04-07 12:33:48 +0300

    Update Vars.js

--- src/util/Vars.js
+++ src/util/Vars.js
@@ -3,7 +3,7 @@
 const Vars = {
 	
 	env: {
-		url: process.env.NODE_ENV == (process.env.URL == undefined ? 'https://github.com/Agzam-Ar/project-2/tree/main/public/' : process.env.URL),
+		url: (process.env.URL == undefined ? 'https://github.com/Agzam-Ar/project-2/tree/main/public/' : process.env.URL),
 	}
 };
 
