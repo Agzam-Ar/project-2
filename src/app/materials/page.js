547c1f6b026b8eeb110ec066ab48c0a5bd8d7ab6
TempUser <temp@mail>
2025-04-27 13:15:37 +0300

    removed debug

--- src/app/materials/page.js
+++ src/app/materials/page.js
@@ -3,5 +3,5 @@
 import styles from "./page.module.css";
 
 export default function Materials() {
-    return <div className={styles.page}>Materials page</div>;
+    return <div className={styles.page}></div>;
 }
